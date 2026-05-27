480b08e0b83e5ebf2e04883b426b7471bacb710d
Jeong Jihyeon <jh.jeong129@gmail.com>
2026-05-27 16:21:41 +0000

    Read Image from floppy done.

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -5,6 +5,15 @@ SECTION .text
 
 jmp 0x07C0:START
 
+;;;;;;;;;;;;;;;;;;;;;;;;;
+; Floppy Disk Parameter for read OS image
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+TOTALSECTORCOUNT:   dw  1024
+SECTORNUMBER:   db  0x02
+HEADNUMBER:     db  0x00
+TRACKNUMBER:    db  0x00
+
 START:
     mov ax, 0x07C0
     mov ds, ax
@@ -21,7 +30,7 @@ START:
 
     cmp si, 80*25*2
 
-    jl .SCREENCLEARLOOP
+    jb .SCREENCLEARLOOP
 
     mov si, 0
     mov di, 0
@@ -43,6 +52,67 @@ START:
     jb .MESSAGELOOP
 
 .MESSAGEEND:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+; Read OS Image Begin
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        mov si, 0x1000
+        mov es, si
+        
+        mov bx, 0x0000
+
+        mov di, word[TOTALSECTORCOUNT]
+
+READDATA:
+    cmp di, 0
+    je READEND
+    sub di, 0x1
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; BIOS Function Calling
+    ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    mov ah, 0x02 ; function num
+    mov al, 0x1 ; read sector count
+    mov ch, byte[TRACKNUMBER]
+    mov cl, byte[SECTORNUMBER]
+    mov dh, byte[HEADNUMBER]
+    mov dl, 0x00
+
+    int 0x13 ; calling interupt service
+
+    jc HANDLEDISKERROR
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Calculating track, head, sector address
+    ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    add si, 0x20
+    mov es, si
+
+    mov al, byte[SECTORNUMBER]
+    add al, 0x1
+
+    mov byte[SECTORNUMBER], al
+    cmp al, 19
+    jb READDATA
+
+    xor byte[HEADNUMBER], 0x01
+    mov byte[SECTORNUMBER], 0x01
+
+    cmp byte[HEADNUMBER], 0x00
+    jne READDATA
+
+    add byte[TRACKNUMBER], 0x01
+    jmp READDATA
+    
+READEND:
+    jmp $
+
+HANDLEDISKERROR:
+    jmp $
+
 
 jmp $
 
